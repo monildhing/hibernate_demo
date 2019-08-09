7124cfd0229f74dbd9cca77ec7bb13fb5eb84ff5
monildhing <49285888+monildhing@users.noreply.github.com>
2019-08-09 11:16:56 +0530

    Update Helloworld.java

--- HIbernate_ex1/src/main/java/com/example/jpahibernate/Helloworld.java
+++ HIbernate_ex1/src/main/java/com/example/jpahibernate/Helloworld.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.example.jpahibernate;
 
 import java.io.Serializable;
@@ -72,7 +68,7 @@ public class Helloworld implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        
         if (!(object instanceof Helloworld)) {
             return false;
         }
